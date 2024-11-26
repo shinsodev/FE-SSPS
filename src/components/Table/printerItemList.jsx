e3131e8695b57fab813e46037b97ea1cd7879db4
vietduc200 <nguyenvietduc2004.cx@gmail.com>
2024-11-27 04:39:02 +0700

    updateEditPrinter2

--- src/components/Table/printerItemList.jsx
+++ src/components/Table/printerItemList.jsx
@@ -43,7 +43,7 @@ function ItemPriter(props) {
 
   const handleStatusChange = async() => {
     const confirmChange = window.confirm(
-      `Are you sure you want to change the status to ${currentStatus === "ONLINE" ? "OFFLINE" : "ONLINE"}?`
+      `Bạn có muốn thay đổi trạng thái máy in thành ${currentStatus === "ONLINE" ? "OFFLINE" : "ONLINE"}?`
     );
     if (confirmChange) {
       //dung try catch 
@@ -62,7 +62,7 @@ function ItemPriter(props) {
         const token = localStorage.getItem('token');
         const response = await enablePrinter(token, printerID);
         console.log("res", response)
-        if (response.data.result === "Printer enabled successfully.") {
+        if (response.data.result === "Printer enabled successfully") {
           setCurrentStatus("ONLINE");
         }
         else {
@@ -114,61 +114,3 @@ export default ItemPriter;
 
 
 
-// import { NavLink } from "react-router-dom";
-// import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
-// import { useEffect } from "react";
-// function ItemPriter(props) {
-
-//   const { printerID, printerLocation, availableDocType, papersLeft, status } = props.item;
-//   const { editPrinter } = props;
-
-//   //
-//   function handleEditPrinter() {
-//     const printer = {
-//       printerID: printerID,
-//       printerLocation: printerLocation,
-//       availableDocType: availableDocType,
-//       papersLeft: papersLeft,
-//       status: status
-//     }
-//     editPrinter(printer );
-//   }
-// //
-//   useEffect(() => {
-//     console.log("oke", printerID)
-//   }, [])
-//   return (
-//     <tr className="bg-white border-b hover:bg-gray-50">
-//       <td className="px-6 py-4">{printerID}</td>
-//       <td className="px-6 py-4">{printerLocation}</td>
-//       <td className="px-6 py-4">{availableDocType.join("/")}</td>
-//       <td className="px-6 py-4">
-//         {papersLeft}
-//         {/* <img className="w-10 h-10" src={imgSrc} alt="" /> */}
-//       </td>
-//       <td className="px-6 py-4">
-//         <div className="flex items-center">
-//           <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>{" "}
-//           {status}
-//         </div>
-//       </td>
-//       <td className="px-6 py-4 text-center">
-//         <div className="flex items-center gap-3">
-//           <NavLink to="#" type="button" className="font-medium text-indigo-500">
-//             <FaEye size={20} />
-//           </NavLink>
-//           <button
-//             className="font-medium text-green-500"
-//             onClick={handleEditPrinter}
-//           >
-//             <FaEdit size={20} />
-//           </button>
-//           <button className="font-medium text-red-500">
-//             <FaTrash size={20} />
-//           </button>
-//         </div>
-//       </td>
-//     </tr>
-//   );
-// }
-// export default ItemPriter;
